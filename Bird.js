d855c00104647d378b0bf348e4782398363b41ae
Om Tejaswi <omtejaswi2008classes@gmail.com>
2021-01-27 14:04:57 +0530

    updated

--- Bird.js
+++ Bird.js
@@ -2,8 +2,24 @@ class Bird extends BaseClass{
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+
+    this.Xarray = []
+    this.Yarray = []
   }
   display(){
     super.display();
+
+    var birdX = [this.body.position.x]
+    var birdY = [this.body.position.y]
+
+    this.Xarray.push(birdX);
+    this.Yarray.push(birdY);
+
+    for(var i = 0; i< this.Xarray.length; i++) {
+          ellipseMode(RADIUS);
+          fill("white");
+          noStroke();
+          ellipse(this.Xarray[i], this.Yarray[i],2);
+    }
   }
 }
